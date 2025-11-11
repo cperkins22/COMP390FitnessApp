d28ad55cbcb9bcd71668056c792ddf183932d685
cperkins22 <c1perkins@student.bridgew.edu>
2025-11-11 11:48:51 -0500

    -Connected trackmeal and trackworkout UI, changed a bit of UI -Created trackmealcontroller and trackworkoutcontroller

---
+++ src/main/java/com/fitnessapp/MainMenuController.java
@@ -0,0 +1,53 @@
+package com.fitnessapp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainMenuController {
+
+    @FXML
+    private void handleSettingsButton(ActionEvent event) throws IOException {
+        // Load the main menu FXML
+        Parent settingsParent = FXMLLoader.load(getClass().getResource("/fxml/settings.fxml"));
+        Scene settingsScene = new Scene(settingsParent);
+
+        // Get the current stage (window) and set the new scene
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(settingsScene);
+        stage.setTitle("Settings");
+        stage.show();
+    }
+
+    @FXML
+    private void handleTrackWorkoutButton(ActionEvent event) throws IOException {
+        // Load the main menu FXML
+        Parent workoutParent = FXMLLoader.load(getClass().getResource("/fxml/TrackWorkout.fxml"));
+        Scene workoutScene = new Scene(workoutParent);
+
+        // Get the current stage (window) and set the new scene
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(workoutScene);
+        stage.setTitle("Workout");
+        stage.show();
+    }
+    @FXML
+    private void handleTrackMealButton(ActionEvent event) throws IOException {
+        // Load the main menu FXML
+        Parent mealParent = FXMLLoader.load(getClass().getResource("/fxml/TrackMeal.fxml"));
+        Scene mealScene = new Scene(mealParent);
+
+        // Get the current stage (window) and set the new scene
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(mealScene);
+        stage.setTitle("Meal Plan");
+        stage.show();
+    }
+
+}
---
+++ src/main/java/com/fitnessapp/SettingsController.java
@@ -0,0 +1,28 @@
+package com.fitnessapp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SettingsController {
+
+    @FXML
+    private void handleBackButton(ActionEvent event) throws IOException {
+        // Load the main menu FXML
+        Parent mainMenuParent = FXMLLoader.load(getClass().getResource("/fxml/mainmenu.fxml"));
+        Scene mainMenuScene = new Scene(mainMenuParent);
+
+        // Get the current stage (window) and set the new scene
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(mainMenuScene);
+        stage.setTitle("Main Menu");
+        stage.show();
+    }
+
+}
