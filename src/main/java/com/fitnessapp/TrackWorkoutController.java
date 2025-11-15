2fa4a1910db5a73043553980df2915333eab5303
Cashstu <cstuger@student.bridgew.edu>
2025-11-15 13:04:59 -0500

    Revert "Workout Section Re-work:"
    This reverts commit dc449e11158521f4f57179395f06ae63d350fcf0.

--- src/main/java/com/fitnessapp/TrackWorkoutController.java
+++ src/main/java/com/fitnessapp/TrackWorkoutController.java
@@ -16,27 +16,27 @@ import java.io.IOException;
 
 public class TrackWorkoutController {
 
-//    @FXML private Spinner<Integer> setsSpinner;
-//    @FXML private TextField exerciseNameInput;
-//    @FXML private VBox exercisesContainer;
-//
-//    @FXML
-//    public void initialize() {
-//        SpinnerValueFactory<Integer> valueFactory =
-//                new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 20, 3);
-//        setsSpinner.setValueFactory(valueFactory);
-//    }
+    @FXML private Spinner<Integer> setsSpinner;
+    @FXML private TextField exerciseNameInput;
+    @FXML private VBox exercisesContainer;
+
+    @FXML
+    public void initialize() {
+        SpinnerValueFactory<Integer> valueFactory =
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 20, 3);
+        setsSpinner.setValueFactory(valueFactory);
+    }
 
     @FXML
     private void handleBackButton(ActionEvent event) throws IOException {
         // Load the main menu FXML
-        Parent workoutIntermediateParent = FXMLLoader.load(getClass().getResource("/fxml/workout_intermediate.fxml"));
-        Scene workoutIntermediateScene = new Scene(workoutIntermediateParent);
+        Parent mainMenuParent = FXMLLoader.load(getClass().getResource("/fxml/mainmenu.fxml"));
+        Scene mainMenuScene = new Scene(mainMenuParent);
 
         // Get the current stage (window) and set the new scene
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(workoutIntermediateScene);
-        stage.setTitle("Workouts");
+        stage.setScene(mainMenuScene);
+        stage.setTitle("Main Menu");
         stage.show();
     }
 
